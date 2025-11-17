99e206b56e19c4eb6f5ff15ab6e80b8d418cae99
Subhranil Ghoshal <ghoshalsubhranil@gmail.com>
2025-11-17 20:31:32 +0530

    Implemented standard divider

--- SV/divider.sv
+++ SV/divider.sv
@@ -1,116 +1,104 @@
 `timescale 1ns/1ps
 
-// module divider #(
-// 	// lengths of all registers
-// 	parameter integer ldvn = 8,
-// 	parameter integer ldvr = 8,
-// 	parameter integer lqtn = 8,
-// 	parameter integer lrem = 8
-// ) (
-// 	input clk,
-// 	input rst,		// active low rst
-	
-// 	input [ldvn-1:0] dividend,
-// 	input [ldvr-1:0] divisor,
-	
-// 	output reg [lqtn-1:0] quotient,
-// 	output reg [lrem-1:0] remainder,
-
-// 	output [ldvn-1:0] dvsr_debug,
-	
-// 	output done,
-// 	output error		// only triggered when divisor is 0
-// );
-
-// 	localparam integer	counter_size = $clog2(ldvr);
-
-// 	reg	[counter_size-1:0]	counter;
-// 	reg				en = 1'b1;
-
-// 	reg	[ldvr-1:0]		dvsr;
-// 	wire	[lrem-1:0]		temp;
-// 	wire				temp_bit;
-
-// 	assign 				done = ~en;
-// 	assign				error = ~(|divisor);
-// 	assign				dvsr_debug = dvsr;
-
-// 	// Logic for enable and counter
-// 	always @(posedge clk or negedge rst) begin
-// 		if(~rst) begin
-// 			en		<= 1'b1;
-// 			counter		<= {counter_size{1'b0}};
-// 			quotient	<= {lqtn{1'b0}};
-// 			remainder	<= dividend;
-// 			dvsr		<= divisor;
-// 		end
-		
-// 		else if(error) begin
-// 			en	<= 1'b0;
-// 			counter	<= {counter_size{1'b0}};
-// 		end
-
-// 		else if(en) begin
-// 			if(counter == lqtn+1) begin
-// 				counter	<= {counter_size{1'b0}};
-// 				en	<= 1'b0;
-// 			end
-// 			else
-// 				counter	<= counter+1;
-// 		end
-// 	end
-// 	// division logic
-// 	RCA_SUB #(.N(lrem)) subtractor (.A(remainder), .B(dvsr), .Bin(1'b0), .D(temp), .Bout(temp_bit));
-// 	always @(posedge clk or negedge rst) begin
-// 		if(en) begin
-// 			quotient <= {quotient[lqtn-2:0], ~temp_bit};
-// 			remainder <= (temp_bit)?(remainder):(temp);
-// 			dvsr <= {1'b0, dvsr[ldvr-1:1]};
-// 		end
-// 	end
-
-// endmodule
-
-`timescale 1ns/1ps
-
 module divider #(
-	parameter N = 4
+	// lengths of all registers
+	parameter integer N = 4
 ) (
-	input		clk, rst,
-	input		[N-1:0]	divisor,  dividend,
-	output	reg	[N-1:0]	quotient,
-	output	reg	[N-1:0]	remainder,
-	output 		done, error
+	input clk, rst,
+	input [N-1:0] divisor, dividend,
+	output reg [N-1:0] quotient = 0,
+	output reg [2*N-1:0] remainder = 0,
+	output done, error
 );
-	reg [N-1:0] rem;
+	localparam integer counter_size = $clog2(N)+1;
 
-	reg en = 1'b1;
-	assign done = ~en;
-	assign error = ~(|divisor);
+	reg	[counter_size-1:0]	counter = {counter_size{1'b0}};
+	reg				en = 1'b1;
 
-	wire [N-1:0] q_in, r_in;
-	wire q_cout, r_bout;
+	reg	[2*N-1:0]		dvsr;
+	wire	[2*N-1:0]		temp;
+	wire				temp_bit;
 
-	RCA #(.N(N)) adder(.A(quotient), .B({N{1'b0}}), .Cin(1'b1), .S(q_in), .Cout(q_cout));
-	RCA_SUB #(.N(N)) subtractor(.A(remainder), .B(divisor), .Bin(1'b0), .D(r_in), .Bout(r_bout));
+	assign 				done = ~en;
+	assign				error = ~(|divisor);
 
+	// Logic for enable and counter
 	always @(posedge clk or negedge rst) begin
+// 		if(counter == 0)
+//     $display("temp_bit FIRST CYCLE = %b  temp = %b", temp_bit, temp);
 		if(~rst) begin
-			en <= 1'b1;
-			quotient <= {N{1'b0}};
-			remainder <= dividend;
+			en		<= 1'b1;
+			counter		<= {counter_size{1'b0}};
+			quotient	<= {N{1'b0}};
+			remainder	<= {{N{1'b0}}, dividend};
+			dvsr		<= {divisor, {N{1'b0}}};
 		end
-		else if (error) begin
-			en <= 1'b0;
+		
+		else if(error) begin
+			en	<= 1'b0;
+			counter	<= {counter_size{1'b0}};
 			quotient <= {N{1'b0}};
-			remainder <= {N{1'b0}};
+			remainder <= {2*N{1'b0}};
 		end
+
 		else if(en) begin
-			if(r_bout) en <= 1'b0;
-			else begin
-				quotient <= q_in;
-				remainder <= r_in;
-			end
+			if(counter == N) en <= 1'b0;
+			counter	<= counter+1;
 		end
 	end
+
+	// division logic
+	RCA_SUB #(.N(2*N)) subtractor (.A(remainder), .B(dvsr), .Bin(1'b0), .D(temp), .Bout(temp_bit));
+	always @(posedge clk or negedge rst) begin
+		if(en) begin
+			quotient <= {quotient[N-2:0], ~temp_bit};
+			remainder <= (temp_bit)?(remainder):(temp);
+			dvsr <= {1'b0, dvsr[2*N-1:1]};
+		end
+	end
+
 endmodule
+
+// `timescale 1ns/1ps
+
+// module divider #(
+// 	parameter N = 4
+// ) (
+// 	input		clk, rst,
+// 	input		[N-1:0]	divisor,  dividend,
+// 	output	reg	[N-1:0]	quotient,
+// 	output	reg	[N-1:0]	remainder,
+// 	output 		done, error
+// );
+// 	reg [N-1:0] rem;
+
+// 	reg en = 1'b1;
+// 	assign done = ~en;
+// 	assign error = ~(|divisor);
+
+// 	wire [N-1:0] q_in, r_in;
+// 	wire q_cout, r_bout;
+
+// 	RCA #(.N(N)) adder(.A(quotient), .B({N{1'b0}}), .Cin(1'b1), .S(q_in), .Cout(q_cout));
+// 	RCA_SUB #(.N(N)) subtractor(.A(remainder), .B(divisor), .Bin(1'b0), .D(r_in), .Bout(r_bout));
+
+// 	always @(posedge clk or negedge rst) begin
+// 		if(~rst) begin
+// 			en <= 1'b1;
+// 			quotient <= {N{1'b0}};
+// 			remainder <= dividend;
+// 		end
+// 		else if (error) begin
+// 			en <= 1'b0;
+// 			quotient <= {N{1'b0}};
+// 			remainder <= {N{1'b0}};
+// 		end
+// 		else if(en) begin
+// 			if(r_bout) en <= 1'b0;
+// 			else begin
+// 				quotient <= q_in;
+// 				remainder <= r_in;
+// 			end
+// 		end
+// 	end
+// endmodule
--- SV/divider_tb.sv
+++ SV/divider_tb.sv
@@ -2,33 +2,35 @@
 
 module divider_tb();
 
-    reg [7:0] dividend = 8'd225;
-    reg [7:0] divisor = (8'd0);
-    wire [7:0] quotient;
-    wire [7:0] remainder;
-    reg clk = 1'b1;
-    reg rst = 1'b1;
-    wire done, error;
+	reg [3:0] dividend = 4'd15;
+	reg [3:0] divisor = 4'd7;
+	wire [3:0] quotient;
+	wire [7:0] remainder;
+	wire [3:0] dbg;
+	reg clk = 1'b0;
+	reg rst = 1'b1;
+	wire done, error;
 
-    initial begin
-        $monitor("%d %d %d %d\tError: %b", divisor, dividend, quotient, remainder, error);
-    end
+	initial begin
+		forever begin
+			#0.5 clk = ~clk;
+		end
+	end
 
-    initial begin
-        forever begin
-            #0.5 clk = ~clk;
-        end
-    end
+	divider #(.N(4)) dut (.clk(clk), .rst(rst), .dividend(dividend), .divisor(divisor), .quotient(quotient), .remainder(remainder), .done(done), .error(error));
 
-    // divider #(.ldvn(8), .ldvr(8), .lqtn(8), .lrem(8)) dut (.clk(clk), .rst(rst), .dividend(dividend), .divisor(divisor), .quotient(quotient), .remainder(remainder), .done(done), .error(error), .dvsr_debug(dvsr_debug));
-
-    divider #(.N(8)) dut(.clk(clk), .rst(rst), .divisor(divisor), .dividend(dividend), .quotient(quotient), .remainder(remainder), .done(done), .error(error));
-
-    initial begin
-        #1 rst = 1'b1;
-        #1 rst = 1'b0;
-        #1 rst = 1'b1;
-        #1000 $finish;
-    end
+	initial begin
+		for(integer i=0; i<16; i=i+1) begin
+			for(integer j=0; j<16; j=j+1) begin
+				divisor = i;
+				dividend = j;
+				@(posedge clk); rst = 1'b0;
+				@(posedge clk);	rst = 1'b1;
+				wait(done);
+				$display("Divisor: %d\tDividend: %d\tQuotient: %d\tRemainder: %d\tError: %b\tDone: %b", divisor, dividend, quotient, remainder, error, done);
+			end
+		end
+		$finish;
+	end
 
 endmodule
