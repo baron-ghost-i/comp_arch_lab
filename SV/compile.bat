ddf4fb0fd74727de063e2a69f2d8fedd92f147ed
Subhranil Ghoshal <ghoshalsubhranil@gmail.com>
2025-11-17 13:26:15 +0530

    Added support for a 3rd argument

--- SV/compile.bat
+++ SV/compile.bat
@@ -1,8 +1,13 @@
 @echo off
 
-IF "%2"=="" (
-	iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv
+IF "%3"=="" (
+	IF "%2"=="" (
+		iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv
+	) ELSE (
+		iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv %2.sv
+	)
 ) ELSE (
-	iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv %2.sv
+	iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv %2.sv %3.sv
+
 )
 vvp output/%1.out
