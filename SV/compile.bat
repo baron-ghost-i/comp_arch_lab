3d93557ea24a5c5a63b8b94a4368ee386617001c
Subhranil Ghoshal <ghoshalsubhranil@gmail.com>
2025-11-18 11:43:39 +0530

    Added support for 4 arguments

--- SV/compile.bat
+++ SV/compile.bat
@@ -1,13 +1,17 @@
 @echo off
 
-IF "%3"=="" (
-	IF "%2"=="" (
-		iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv
+IF "%4"=="" (
+	IF "%3"=="" (
+		IF "%2"=="" (
+			iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv
+		) ELSE (
+			iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv %2.sv
+		)
 	) ELSE (
-		iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv %2.sv
+		iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv %2.sv %3.sv
+
 	)
 ) ELSE (
-	iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv %2.sv %3.sv
-
+		iverilog -g2012 -o output/%1.out adders.sv %1.sv %1_tb.sv %2.sv %3.sv %4.sv
 )
 vvp output/%1.out
